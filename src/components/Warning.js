abdab32ec74a109e31fff3a826018209f1e0f8f1
Phynnex <angalafynetonye@gmail.com>
2022-03-24 19:22:55 +0100

    worked on forcast

---
+++ src/components/Warning.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "../styles/Warning.css";
+// import London from "./London";
+
+function Warning() {
+  return (
+    <div className="Warning">
+      
+      <h2> No location found </h2>
+      
+    </div>
+  );
+}
+
+export default Warning;
